013d6888d84b4619a95e75ddc54be89296d140ad
Aman <aman.prajapati7022@gmail.com>
2023-06-28 10:53:21 +0530

    Add operate funtion

--- code.js
+++ code.js
@@ -2,14 +2,20 @@ const addition = (a, b) => (+a)+(+b);
 const subtraction = (a, b) => a-b;
 const multiply = (a, b) => a*b;
 const divide = (a, b)=>{
-    if (b == 0) return "division by Zero";
+    if (b == 0) return "OOPS";
     return a/b;
 };
 
-console.log(addition(6,7));
-console.log(subtraction(3,7));
-console.log(multiply(3,7));
-console.log(divide(3,0));
-console.log(divide(22,7));
+let no1 = 8;
+let operator = "+";
+let no2=6;
 
+let operate = function(operator, no1, no2){
+    if (operator == "+") return addition(no1, no2);
+    else if (operator == "-") return subtraction(no1, no2);
+    else if (operator == "*") return multiply(no1, no2);
+    else if (operator == "/") return divide(no1, no2);
+    return "Not a valid operator"
+}
 
+console.log(operator("0/9"));
